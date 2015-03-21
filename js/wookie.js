a84272d26a37782cf8d22d167ccc78ccf5118167
Techcovered <abhilashr@outlook.com>
2015-03-21 22:41:16 +0530

    Added site color scheme

--- js/wookie.js
+++ js/wookie.js
@@ -40,8 +40,36 @@
         // You can put any text, image or even IFRAME tags here
         ad.innerHTML = '<img src="http://www.techcovered.org/images/adblock-plead.png"/>';
         //document.getElementById("showadblock").innerHTML='<img src="http://img.techcovered.org/adblock.png"/>';
-        console.log("Executed3");
       }
       
     }, 2000); // The ad blocker check is performed 2 seconds after the page load 
   }).call(this); 
+
+
+(function() {
+	//Function to change color scheme on reload
+		var colors = ["0080AA","#1E7145","#00aba9","#603CBA","#000000","#B91D47","#2B5797","#FE7C22"];
+	//To find random
+	 	var colorindex = Math.floor((Math.random() * 10) + 1); 
+	 	while(colorindex>7||colorindex<1) {
+	 		colorindex = Math.floor((Math.random() * 10) + 1);
+	 	}
+	 	console.log(colorindex);
+		$(".container-fluid.header").css("background-color", colors[colorindex]);
+		$(".col-xs-10 > h2  a").css("color", colors[colorindex]);
+		$(".pagination > ul > li a").css("background-color", colors[colorindex]);
+		$(".pagination > ul > li a").css("border-color", colors[colorindex]);
+		$(".pagination > ul > li.active").css("border-color", colors[colorindex]);
+		$(".pagination > ul > li.active").css("color", colors[colorindex]);
+		$("footer > span > a").css("color", colors[colorindex]);
+		$(".button").css("background-color", colors[colorindex]);
+		$(".button").css("border-color", colors[colorindex]);
+		$("#thepost a").css("color", colors[colorindex]);
+		$("nav#slide-menu").css("background-color", colors[colorindex]);
+		$("#mc_embed_signup").css("background-color", colors[colorindex]);
+		$("#relatedones").css("background-color", colors[colorindex]);
+		$(".mc-field-group > input").css("background-color", colors[colorindex]);
+		$("<style> ::selection{ background-color:"+colors[colorindex]+"; } ::-moz-selection{ background-color:"+colors[colorindex]+"; }</style>").insertAfter("body *:last");
+
+
+}).call(this);
